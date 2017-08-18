b45b9d4dd493e6f5f674f9303a12139fb28be13e
qupeng <onlyqupeng@gmail.com>
2017-08-18 16:56:23 +0800

    mysqlx: add mysqlx proto files and generate code. (#37)

---
+++ generate-mysqlx.sh
@@ -0,0 +1,11 @@
+cd proto
+
+echo "generate go code..."
+GOGO_ROOT=${GOPATH}/src/github.com/gogo/protobuf
+find ./Mysqlx -name "*.proto" -exec protoc -I.:${GOGO_ROOT}:${GOGO_ROOT}/protobuf --gofast_out=.. {} \;
+cd ..
+find ./Mysqlx -name "*.pb.go" -exec sed -i -E 's/import _ \"gogoproto\"//g' {} \;
+find ./Mysqlx -name "*.pb.go" -exec sed -i -E 's/import fmt \"fmt\"//g' {} \;
+find ./Mysqlx -name "*.pb.go" -exec sed -i -E 's/Mysqlx\//github.com\/pingcap\/tipb\/go-mysqlx\//g' {} \;
+find ./Mysqlx -name "*.pb.go" -exec goimports -w {} \;
+rm -rf go-mysqlx && mv Mysqlx go-mysqlx
