a4cb837b2f797f54d3b2bbb3da47dd15af14de7d
Huachao Huang <huachao.huang@gmail.com>
2018-03-14 10:56:12 +0800

    Add checksum request (#78)

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate protobuf;
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub mod analyze;
 #[cfg_attr(rustfmt, rustfmt_skip)]
+pub mod checksum;
+#[cfg_attr(rustfmt, rustfmt_skip)]
 pub mod executor;
 #[cfg_attr(rustfmt, rustfmt_skip)]
 pub mod expression;
