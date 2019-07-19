1b33604708b0c57bd0af20a99ac2896aef4f80cc
Jay <BusyJay@users.noreply.github.com>
2019-07-19 19:10:33 +0800

    make clippy happy (#127)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#[allow(dead_code)]
+#[allow(unknown_lints)]
+#[allow(clippy::all)]
+#[allow(renamed_and_removed_lints)]
+#[allow(bare_trait_objects)]
 mod protos {
     include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 }
