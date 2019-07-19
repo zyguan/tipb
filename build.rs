bbc447d031c9049e43399d1794c9b1e9bad16a8c
Jay <BusyJay@users.noreply.github.com>
2019-07-19 18:01:49 +0800

    support rust-protobuf by default (#126)
    * support rust-protobuf by default
    
    and optionally support prost.
    
    Note that prost is broken currently, which should be fixed
    later.
    
    * fix prost
    
    * update hash

--- build.rs
+++ build.rs
@@ -11,66 +11,51 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#[cfg(feature = "gen")]
-use std::fs::File;
-#[cfg(feature = "gen")]
-use std::io::Write;
+use std::fs::{self, File};
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+const BLACK_LIST: &[&str] = &["protobuf", "google", "gogoproto"];
 
 fn main() {
-    if !cfg!(feature = "gen") {
-        println!("cargo:rerun-if-changed=build.rs");
-        return;
-    }
-
-    #[cfg(feature = "gen")]
-    {
-        use protobuf_build::*;
-        use std::fs;
-
-        check_protoc_version();
-
-        let mut mods: Vec<_> = fs::read_dir("proto")
-            .expect("Couldn't read directory")
-            .filter_map(|e| {
-                let file_name = e.expect("Couldn't list file").file_name();
-                let file_name = file_name.to_string_lossy();
-                if !file_name.ends_with(".proto") {
-                    return None;
+    let out_dir = format!("{}/protos", std::env::var("OUT_DIR").unwrap());
+    let mods: Vec<_> = fs::read_dir("proto")
+        .expect("Couldn't read directory")
+        .filter_map(|e| {
+            let file_name = e.expect("Couldn't list file").file_name();
+            let file_name = file_name.to_string_lossy();
+            if !file_name.ends_with(".proto") {
+                return None;
+            }
+            Some(format!("proto/{}", file_name))
+        })
+        .collect();
+    protobuf_build::generate_files(&["include".to_owned(), "proto".to_owned()], &mods, &out_dir);
+    let mod_file_path = Path::new(&out_dir).join("mod.rs");
+    let mod_file = File::open(&mod_file_path).unwrap();
+    let reader = BufReader::new(mod_file);
+    let mut content = Vec::new();
+    let mut lines = reader.lines();
+    while let Some(l) = lines.next() {
+        let l = l.unwrap();
+        if BLACK_LIST.iter().any(|i| l.contains(i)) {
+            let mut level = 1;
+            while level > 0 {
+                let l = lines.next().unwrap().unwrap();
+                if l.contains('{') {
+                    level += 1;
                 }
-                Some(file_name[..file_name.len() - 6].to_owned())
-            })
-            .collect();
-        mods.sort();
-        let protos: Vec<_> = mods.iter().map(|m| format!("proto/{}.proto", m)).collect();
-
-        for p in &protos {
-            println!("cargo:rerun-if-changed={}", p);
+                if l.contains('}') {
+                    level -= 1;
+                }
+            }
+        } else {
+            content.push(l);
         }
-
-        // Rust-protobuf
-        generate_protobuf_files(&protos, "src/protobuf");
-        replace_read_unknown_fields(&protos);
-        generate_lib_file(&mods);
-
-        // Prost
-        generate_prost_files(&protos, "src/prost");
-        generate_wrappers(&["src/prost/tipb.rs"], "src/prost", GenOpt::all());
-        fs::remove_file("src/prost/gogoproto.rs").unwrap();
-        fs::remove_file("src/prost/google.protobuf.rs").unwrap();
     }
-}
-
-#[cfg(feature = "gen")]
-fn generate_lib_file<T: AsRef<str>>(mod_names: &[T]) {
-    let mut text = String::new();
-
-    for mod_name in mod_names {
-        text.push_str("#[rustfmt::skip]\npub mod ");
-        text.push_str(mod_name.as_ref());
-        text.push_str(";\n");
+    let mut mod_file = File::create(&mod_file_path).unwrap();
+    for l in content {
+        writeln!(mod_file, "{}", l).unwrap();
     }
-
-    let mut lib = File::create("src/protobuf.rs").expect("Could not create protobuf.rs");
-    lib.write_all(text.as_bytes())
-        .expect("Could not write protobuf.rs");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,5 @@
-#[cfg(feature = "prost-codec")]
-pub use crate::prost::*;
-#[cfg(feature = "protobuf-codec")]
-pub use crate::protobuf::*;
-
-#[cfg(feature = "protobuf-codec")]
-mod protobuf;
-#[cfg(feature = "prost-codec")]
-mod prost {
-    include!("prost/tipb.rs");
-    include!("prost/wrapper_tipb.rs");
+mod protos {
+    include!(concat!(env!("OUT_DIR"), "/protos/mod.rs"));
 }
+
+pub use crate::protos::*;
