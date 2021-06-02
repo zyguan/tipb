92b010b1638943e29217deb55d23460c6485da6a
Yilong Li <liyilongko@gmail.com>
2021-06-02 20:05:23 +0800

    *: add top sql agent service definition (#225)
    * *: add top sql agent service definition
    
    * add plugin=grpc in generating script
    
    * change to proto3
    
    * change rpc to unary responze, remove tikv rpc
    
    * fix generated empty.go import
    
    * add comments
    
    * re-generate go file
    
    * keep align with kvproto
    
    * Update proto/topsql_agent.proto
    
    Co-authored-by: Wenxuan <hi@breeswish.org>
    
    * fix
    
    * fix generated code format manually
    
    * add field
    
    * upgrade to go 1.16
    
    * format
    
    * change back to go 1.13, manually modify go-fmted file
    
    * upgrade to go 1.16
    
    * re-generate go file
    
    * fix proto file indentation
    
    * go mod tidy
    
    * use protoc 3.5
    
    * change sql/plan digest to bytes
    
    Co-authored-by: Wenxuan <hi@breeswish.org>

--- generate-go.sh
+++ generate-go.sh
@@ -5,7 +5,9 @@ set -ex
 cd proto
 echo "generate go code..."
 go install github.com/gogo/protobuf/protoc-gen-gofast
-protoc -I.:${GOGO_PROTOBUF} --gofast_out=../go-tipb *.proto
+protoc -I.:${GOGO_PROTOBUF} \
+    --gofast_out=plugins=grpc:../go-tipb *.proto
+
 cd ../go-tipb
 sed -i.bak -E 's/import _ \"gogoproto\"//g' *.pb.go
 sed -i.bak -E 's/import fmt \"fmt\"//g' *.pb.go
--- go-tipb/analyze.pb.go
+++ go-tipb/analyze.pb.go
@@ -13,6 +13,7 @@
 		resourcetag.proto
 		schema.proto
 		select.proto
+		topsql_agent.proto
 		trace.proto
 
 	It has these top-level messages:
@@ -68,6 +69,8 @@
 		ChunkMemoryLayout
 		UserIdentity
 		StreamResponse
+		CollectCPUTimeRequest
+		CollectCPUTimeResponse
 */
 package tipb
 
